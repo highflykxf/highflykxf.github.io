48a63e8d998818886b963866dc657fd53a0ed468
highfly <xiangfei0919@gmail.com>
2017-12-16 18:51:55 +0800

    Site updated: 2017-12-16 18:51:53

--- photos/ins.js
+++ photos/ins.js
@@ -116,8 +116,8 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'https://raw.githubusercontent.com/highflykxf/Blog-Back-Up/blob/master/min_photos/' + data.link[i];
-          var src = 'https://raw.githubusercontent.com/highflykxf/Blog-Back-Up/blob/master/photos/' + data.link[i];
+          var minSrc = 'https://raw.githubusercontent.com/highflykxf/Blog-Back-Up/master/min_photos/' + data.link[i];
+          var src = 'https://raw.githubusercontent.com/highflykxf/Blog-Back-Up/master/photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '.jpg';
